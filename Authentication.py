a8721d72ac2ea504290fd5706ecbfc0702faaa10
sai97000 <msaikumarreddy00@gmail.com>
2018-12-18 14:25:24 +0530

    Update Authentication.py

--- Authentication.py
+++ Authentication.py
@@ -1,29 +1,34 @@
 import random
 import string
-a=open("user.txt","r")
 
-def pwd():
-	pawd=string.ascii_letters + string.digits
-	return ''.join(random.choice(pawd) for x in range(0,8))
-	
+a = open("user.txt", "r")
+
+def pwd():              #Generate Random password
+    pawd = string.ascii_letters + string.digits
+    return ''.join(random.choice(pawd) for x in range(0, 8))
+    
+        #User Verfication
+
 def uname():
-	user=input("enter  user name : ")
-	for line in a.readlines():
-		print(line)
-		print("teting")
-		b=line.split(":")
-		euname=b[0]
-		print(euname)
-		if user==euname:
-			print("user name already exists")
-			break
-		else:
-			adduname(user)
+    user = input("enter  user name : ")
+    for line in a.readlines():
+        b = line.split(":")
+        euname = b[0]
+        if user == euname:
+            print("user name already exists")
+            break
+        else:
+            adduname(user)
+            break
+            
+#If user is not present add him. Called from Verification function.
 
 def adduname(user):
-	a=open("user.txt","a")
-	a.writelines(user+" : "+ pwd())
-	print("user registered")
+    a = open("user.txt", "a")
+    b= user + " : " + pwd()
+    a.writelines(b + "\n")
+    print("user registered")
+
 
 uname()
 
