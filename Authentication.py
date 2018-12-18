d4bbb03edd5ef41355a36522e29287c80f972900
sai97000 <msaikumarreddy00@gmail.com>
2018-12-18 20:03:56 +0530

    General Authentication Complete Working.
    General Authentication. If no user found it will add or else will give an error stating User already existing.

--- Authentication.py
+++ Authentication.py
@@ -2,30 +2,35 @@ import random
 import string
 
 a = open("user.txt", "r")
+user = input("enter  user name : ").lower()
 
-def pwd():              #Generate Random password
+
+def pwd():  # Generate Random password
     pawd = string.ascii_letters + string.digits
     return ''.join(random.choice(pawd) for x in range(0, 8))
-    
-        #User Verfication
+
+    # User Verfication
 
 def uname():
-    user = input("enter  user name : ")
     for line in a.readlines():
         b = line.split(":")
         euname = b[0]
         if user == euname:
-            print("user name already exists")
-            break
+            present = 1
         else:
-            adduname(user)
-            break
-            
-#If user is not present add him. Called from Verification function.
+            present = 0
+
+    if present == 1:
+        print("user name already exists")
+    else:
+        adduname(user)
+
+
+# If user is not present add him. Called from Verification function.
 
 def adduname(user):
     a = open("user.txt", "a")
-    b= user + " : " + pwd()
+    b = user + ": " + pwd()
     a.writelines(b + "\n")
     print("user registered")
 
